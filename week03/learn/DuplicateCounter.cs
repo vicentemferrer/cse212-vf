0e4fdefeb3359ac0712a693bcd0b9f67fac5c0e6
Vicente <vicenteferrer_01@hotmail.com>
2025-03-18 12:18:47 -0300

    W03 Learn: Sets and Maps

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,8 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        HashSet<int> mySet = new HashSet<int>(data);
+
+        return data.Length - mySet.Count;
     }
 }
--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -25,6 +25,7 @@ public class Translator
     public void AddWord(string fromWord, string toWord)
     {
         // ADD YOUR CODE HERE
+        _words.Add(fromWord, toWord);
     }
 
     /// <summary>
@@ -35,6 +36,6 @@ public class Translator
     public string Translate(string fromWord)
     {
         // ADD YOUR CODE HERE
-        return "";
+        return _words.ContainsKey(fromWord) ? _words[fromWord] : "???";
     }
 }
