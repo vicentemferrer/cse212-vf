d73e5c0f58f819e7b35c86b0d25c4683611b9c3e
Vicente <vicenteferrer_01@hotmail.com>
2025-03-19 22:42:32 -0300

    W03 Code: First 3 Challenges Fulfilled

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using System.Xml.Schema;
 
 public static class SetsAndMaps
 {
@@ -22,7 +23,31 @@ public static class SetsAndMaps
     public static string[] FindPairs(string[] words)
     {
         // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+
+        // Ensure no duplicates
+        var cleanWords = new HashSet<string>(words);
+
+        // Create set
+        var uniquePairs = new HashSet<string>();
+
+        // Loop through the input array, add element to set if it nor its reverse isn't 
+        // stored yet or it isn't a double char string.
+        // Else remove reversed sibling and add to set "{reversed} & {element}" construction.
+        foreach (string word in cleanWords.ToArray()) {
+            var reverse = word.ToCharArray();
+            Array.Reverse(reverse);
+            string reversed = new string(reverse);
+
+            if (!uniquePairs.Contains(word) && !uniquePairs.Contains(reversed) && reverse[0] != reverse[1]) {
+                uniquePairs.Add(word);
+            } else if (reverse[0] != reverse[1]) {
+                uniquePairs.Remove(reversed);
+                uniquePairs.Add($"{reversed} & {word}");
+            }
+        }
+
+        // Return the array filtering potential unhandled strings
+        return Array.FindAll(uniquePairs.ToArray(), (pair) => pair.Length > 2);
     }
 
     /// <summary>
@@ -43,6 +68,16 @@ public static class SetsAndMaps
         {
             var fields = line.Split(",");
             // TODO Problem 2 - ADD YOUR CODE HERE
+
+            // Get degree field to use as key and compare
+            string degree = fields[3];
+
+            // Add key to dictionary if it wasn't there, otherwise, increment.
+            if (!degrees.ContainsKey(degree)) {
+                degrees[degree] = 1;
+            } else {
+                degrees[degree] += 1;
+            }
         }
 
         return degrees;
@@ -67,7 +102,55 @@ public static class SetsAndMaps
     public static bool IsAnagram(string word1, string word2)
     {
         // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+
+        // Turn to lower case and avoid spaces
+        string lower1 = string.Join(string.Empty, word1.ToLower().Split(' '));
+        string lower2 = string.Join(string.Empty, word2.ToLower().Split(' '));
+
+        // Discard cases with different lengths
+        if (lower1.Length != lower2.Length) {
+            return false;
+        }
+
+        // Create two dictionaries to store letters and occurrences
+        var dict1 = new Dictionary<char, int>();
+        var dict2 = new Dictionary<char, int>();
+
+        // Store letters and their occurrences
+        for (int i = 0; i < lower1.Length; i++) {
+            char letter1 = lower1[i];
+            char letter2 = lower2[i];
+
+            if (!dict1.ContainsKey(letter1)) {
+                dict1[letter1] = 1;
+            } else {
+                dict1[letter1] += 1;
+            }
+        
+
+            if (!dict2.ContainsKey(letter2)) {
+                dict2[letter2] = 1;
+            } else {
+                dict2[letter2] += 1;
+            }
+            
+        }
+
+        // Union dictionary keys to confirm letters equality
+        var union = dict1.Keys.ToHashSet().Union(dict2.Keys.ToHashSet());
+
+        if (dict1.Count != union.ToList().Count || dict2.Count != union.ToList().Count){
+            return false;
+        }
+
+        // Compare occurrences
+        foreach (char letter in dict1.Keys.ToArray()) {
+            if (dict1[letter] != dict2[letter]) {
+                return false;
+            }
+        }      
+        
+        return true;
     }
 
     /// <summary>
